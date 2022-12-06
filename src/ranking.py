c3cfb2defaca630640f510f66e0b09981f236ce7
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2022-12-06 03:54:09 +0000

    Work on RankedSet class

--- src/ranking.py
+++ src/ranking.py
@@ -1,11 +1,23 @@
+import logging
+
+
 class RankedSet:
 
 	# Set of driver IDs (IN ORDER)
 	_data: set[int]
+	_score: int
 
 	def __init__(self, data: set[int]):
-		self._data = data
-		print(data)
+		logging.debug(f'RankedSet created with {len(data)} IDs: {data}')
 
-	def get_score(self):
+		self._data = data
+		self._score = 0
+
+	def calculate_score(self):
+		tmp_score = 0
 		pass
+
+
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(asctime)s - %(message)s')
+	rs = RankedSet(set(range(1, 45)))
