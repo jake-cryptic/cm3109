c23be71dfd3e321fc9cd54dac2f98f1350f8a7f4
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2022-12-07 21:26:08 +0000

    Write a little script to make some graphs

--- src/graph_maker.py
+++ src/graph_maker.py
@@ -1,12 +1,59 @@
 #!/usr/bin/python
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sa import SimulatedAnnealing
 from ranking import RankedSet
 from wmg import WMG
 
 
+def plot(x: list, y: list, fig_name: str, plot_title: str):
+	plt.figure(fig_name)
+	plt.title(plot_title)
+	plt.scatter(x, y, s=1)
+	# plt.savefig('figure1.jpg')
+	plt.show()
+	plt.close()
+
+
+def run_controlled_simulation(parser: WMG, param_to_change: str, param_value):
+	values_dict = {
+		'initial_t': 500,
+		't_length': 200,
+		'max_uphill_moves': 60,
+		'cooling_ratio': 0.99
+	}
+
+	values_dict[param_to_change] = param_value
+	print(f'Running SimulatedAnnealing({values_dict=})')
+
+	sa = SimulatedAnnealing(
+		initial_t=values_dict['initial_t'],
+		t_length=values_dict['t_length'],
+		max_uphill_moves=values_dict['max_uphill_moves'],
+		cooling_ratio=values_dict['cooling_ratio']
+	)
+	sa.set_wmg(parser)
+	sa.initialise_rankings()
+	sa.run_outer()
+
+	return sa.get_best_ranking().score
+
+
 def run_tests(parser: WMG) -> None:
-	pass
+	data = {
+		'x': [],
+		'y': []
+	}
+
+	#for test_initial_t in range(100, 2000, 100):
+	#	data['x'].append(test_initial_t)
+	#	data['y'].append(run_controlled_simulation(parser, 'initial_t', test_initial_t))
+	#plot(data['x'], data['y'], 'Kemeny Score', 'Kemeny Score vs Initial Temperature')
+
+	for test_cooling in range(1, 100):
+		data['x'].append(test_cooling/100)
+		data['y'].append(run_controlled_simulation(parser, 'cooling_ratio', test_cooling/100))
+
+	plot(data['x'], data['y'], 'Kemeny Score', 'Kemeny Score vs Cooling Ratio')
 
 
 def main() -> None:
